5afe4476936afc101fd28551e4166f05252c4e88
ricwilson <richard.a.wilson@microsoft.com>
2025-06-06 14:26:39 -0400

    Update deployment profile variable for clarity in UploadToDevelopment script

--- scripts/UploadToDevelopment.ps1
+++ scripts/UploadToDevelopment.ps1
@@ -4,6 +4,6 @@ $siteFolder = "datatables---site-ucxnn"
 $environmentId = "YOUR-DEVELOPMENT-ENV-ID"  # Replace with your Dev environment ID
 $cloud = "Public"
 $modelType = "Enhanced"
-$deploymentProfile = "dev"
+$deploymentProfile = "development"
 
 & "$PSScriptRoot/utilities/Upload.ps1" -SiteFolder $siteFolder -EnvironmentId $environmentId -Cloud $cloud -ModelType $modelType -DeploymentProfile $deploymentProfile
